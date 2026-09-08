be68c61d3824e06fec14bd0d902d97636240c099
Dj-Won <wangdingju@sjtu.edu.cn>
2026-09-08 13:13:36 +0800

    with vlm

--- scripts/zoom_garden_json.sh
+++ scripts/zoom_garden_json.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python generate_zoom_video.py \
-  --scene-config configs/zoom_video/scenes/kelpies.yaml \
+  --scene-config configs/zoom_video/scenes/apartment.yaml \
   --camera-config configs/zoom_video/cameras/x3.yaml \
-  --color-config configs/zoom_video/colors/multicamera_default.yaml \
-  --camera-json /home/wdj/projects/data-generate/outputs/kelpies_traversal/kelpies/position_0067/lens_0000/camera.json
+  --color-config configs/zoom_video/colors/multicamera_low.yaml \
+  --camera-json /home/wdj/projects/data-generate/outputs/apartment_random/apartment/position_0142/lens_0005/camera.json
